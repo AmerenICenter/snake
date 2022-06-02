ee1e5a35149a6d2fc8a9383fc0dde8ed80a67337
testingwebsitess <21andavi@gmail.com>
2022-06-02 10:10:49 -0500

    snake

--- src/snake/game.py
+++ src/snake/game.py
@@ -1,5 +1,5 @@
 from food import food
-from snake import snake 
+from snake import snake
 
 import random 
 import pygame 
@@ -9,11 +9,15 @@ pygame.init()
 # jash bare bones screen code
 screen = pygame.display.set_mode([500, 500])
 running = True
+snake = snake()
+
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    snake.draw(pygame, screen)
     screen.fill((0, 0, 0))
 
 pygame.quit()
