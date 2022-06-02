abf8439699407bb21e04640d669c67465bdab1d7
Jash Parekh <jashrp2@illinois.edu>
2022-06-02 08:13:30 -0700

    jash - made some changes to game

--- src/snake/game.py
+++ src/snake/game.py
@@ -4,10 +4,19 @@ from snake import snake
 import random 
 import pygame 
 
-pygame.init()
-
 # jash bare bones screen code
+pygame.init()
 screen = pygame.display.set_mode([500, 500])
+width = screen.get_width()
+height = screen.get_height()
+color = (255, 255, 255)
+
+font = pygame.font.SysFont('Corbel', 30)
+easy = font.render('easy', True, color)
+medium = font.render('medium', True, color)
+hard = font.render('hard', True, color)
+
+
 running = True
 while running:
     for event in pygame.event.get():
