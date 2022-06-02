fcbfd28d66b0e2dde0dd54910477d6861547dbc1
testingwebsitess <21andavi@gmail.com>
2022-06-02 12:49:57 -0500

    nice

--- src/snake/food.py
+++ src/snake/food.py
@@ -9,7 +9,6 @@ class food(Sprite):
         self.game_screen = screen
 
     def draw(self):
-        size = 26
+        size = 12
         # self.position = [random.randrange(0, 500-size), random.randrage(0, 500-size)]
-        pygame.draw.rect(self.game_screen, (255,255,255),
-        pygame.rect(int(self.position[0]), int(self.position[1]), size, size))
+        pygame.draw.rect(self.game_screen, (255,255,255), pygame.Rect(int(self.position[0]), int(self.position[1]), size, size))
--- src/snake/game.py
+++ src/snake/game.py
@@ -15,13 +15,13 @@ class Direction(Enum):
 
 # jash bare bones screen code
 pygame.init()
-SCREEN_SIZE = 500
+SCREEN_SIZE = 480
 SCREEN = pygame.display.set_mode([SCREEN_SIZE, SCREEN_SIZE])
 WIDTH = SCREEN.get_width()
 HEIGHT = SCREEN.get_height()
 COLOR = (255, 255, 255)
 BLACK = (0,0,0)
-SNAKE_WIDTH = 26
+SNAKE_WIDTH = 12
 
 FONT = pygame.font.SysFont('Corbel', 30)
 EASY = FONT.render('easy', True, COLOR)
@@ -38,19 +38,27 @@ food_group = pygame.sprite.Group()
     # if collide with self
 title = True
 running = True
-x_change = 10
+x_change = 12
 y_change = 0
 x = 200
 y = 200
 
-snake = snake()
+food_x = 12
+food_y = 12
+
+snake_x = SCREEN_SIZE/2
+snake_y = SCREEN_SIZE/2
+
+# snake = snake()
+
+snake_blocks = []
+snake_length = 1
+
 
 def collide(x,y):
     if x == SCREEN_SIZE - WIDTH or y == SCREEN_SIZE - WIDTH or x == WIDTH or y == WIDTH: 
         print("Collide")
     
-    
-
 while running:
     # if (title): 
     #     pass
@@ -63,37 +71,47 @@ while running:
             # checkKeyPress() 
             if event.key == pygame.K_LEFT:
                 print("Left")
-                x_change = 10
+                x_change = 12
                 y_change = 0
                 x_change = -x_change
             elif event.key == pygame.K_RIGHT:
                 print("ROIGHT")
-                x_change = -10
+                x_change = -12
                 y_change = 0 
                 x_change = -x_change
             elif event.key == pygame.K_UP: 
                 print("UOP")
-                y_change = 10
+                y_change = 12
                 x_change = 0
                 y_change = -y_change
             elif event.key == pygame.K_DOWN:
                 print("DOWN")
-                y_change = -10
+                y_change = -12
                 x_change = 0
                 y_change = -y_change
                 
-    # snake_group.update()
-    # food_group.update()
-    
     collide(x,y)
     SCREEN.fill(BLACK)
-    
-    # for sprite in snake_group.sprites(): 
-    for segment in snake.SNAKE: 
+
+    snake_blocks.append([snake_x, snake_y])
+    if len(snake_blocks) > snake_length:
+        del snake_blocks[0]
+        
+    for segment in snake_blocks: 
         pygame.draw.rect(SCREEN, (255,255,0), pygame.Rect(segment[0], segment[1], SNAKE_WIDTH, SNAKE_WIDTH))
-    for segment in snake.SNAKE: 
-        segment[0] += x_change
-        segment[1] += y_change
+    
+    snake_x += x_change
+    snake_y += y_change
+    
+    food1 = food([food_x, food_y], SCREEN)
+    food1.draw()
+    
+    if int(snake_x) == int(food_x) & int(snake_y) == int(food_y): 
+        snake_length += 1
+        food_x = random.randint(1,39) * 12
+        food_y = random.randint(1,39) * 12 
+        print("FOOD")
+        
     # x += x_change
     # y += y_change
     
