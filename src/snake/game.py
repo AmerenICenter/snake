45675a498321295b5bbd88e049703c00e51af940
testingwebsitess <21andavi@gmail.com>
2022-06-02 10:43:46 -0500

    screen, basic game

--- src/snake/game.py
+++ src/snake/game.py
@@ -14,28 +14,49 @@ class Direction(Enum):
 # jash bare bones screen code
 pygame.init()
 SCREEN = pygame.display.set_mode([500, 500])
-WIDTH = screen.get_width()
-HEIGHT = screen.get_height()
+WIDTH = SCREEN.get_width()
+HEIGHT = SCREEN.get_height()
 COLOR = (255, 255, 255)
+BLACK = (0,0,0)
 
 FONT = pygame.font.SysFont('Corbel', 30)
 EASY = FONT.render('easy', True, COLOR)
 MEDIUM = FONT.render('medium', True, COLOR)
 HARD = FONT.render('hard', True, COLOR)
 
-snake = snake()
+snake_group = pygame.sprite.Group() 
+food_group = pygame.sprite.Group()
 
 def checkKeyPress():
-    keys = pygame.key.get_pressed()
-    keys[]
+    if event.key == pygame.K_LEFT:
+        pass
+    elif event.key == pygame.K_RIGHT:
+        pass
+    elif event.key == pygame.K_UP: 
+        pass 
+    elif event.key == pygame.K_DOWN:
+        pass 
 
+title = True
 running = True
+
 while running:
-    pygame.time.delay(100)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    SCREEN.fill((0, 0, 0))
-    pygame.display.update()
+    if (title): 
+        pass
+    else: 
+        pygame.time.delay(100)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                checkKeyPress() 
+                
+        snake.move() 
+        snake_group.update()
+        food_group.update()
+            
+        SCREEN.fill(BLACK)
+        
+        pygame.display.update()
 
 pygame.quit()
